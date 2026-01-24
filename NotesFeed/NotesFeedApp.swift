85b0b0afd26862aa8a55450f051dfd23fbcfeae6
Ibrahim <ibrahim@klsandbox.com>
2026-01-24 08:26:52 -0800

    Initial Commit

---
+++ NotesFeed/NotesFeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesFeedApp.swift
+//  NotesFeed
+//
+//  Created by Klsandbox on 1/24/26.
+//
+
+import SwiftUI
+
+@main
+struct NotesFeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
